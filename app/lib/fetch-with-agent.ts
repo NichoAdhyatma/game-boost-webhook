d99a142a1f6c081ff98721c583473fdbceeb1b22
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-12-06 11:15:35 +0700

    fix: fetch with agent

--- app/lib/fetch-with-agent.ts
+++ app/lib/fetch-with-agent.ts
@@ -1,14 +1,17 @@
-import https from "https";
+import { Agent } from "undici";
 
-const httpsAgent = new https.Agent({
-  keepAlive: true,
-  rejectUnauthorized: false, 
+const httpsAgent = new Agent({
+  keepAliveTimeout: 10_000,
+  keepAliveMaxTimeout: 15_000,
+  connect: {
+    rejectUnauthorized: false, // sama dengan https.Agent
+  },
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetchWithAgent(url: string, options: any = {}) {
   return fetch(url, {
     ...options,
-    dispatcher: httpsAgent, 
+    dispatcher: httpsAgent, // ini valid untuk Next.js
   });
 }
